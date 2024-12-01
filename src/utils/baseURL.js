c1355b0799876d29a7bb05a612a0d889223b4e8e
Rishi Raj <rishi.rishiraj12@gmail.com>
2024-12-01 23:04:13 +0530

    first commit

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     //return "http://localhost:4000"
-    return "https://book-store-mu-two.vercel.app";
+    return "https://book-store-backend-five-alpha.vercel.app";
 }
 
 export default getBaseUrl;
