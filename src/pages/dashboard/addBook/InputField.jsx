abb4eec21905f34243ac00c3f004c00e98a73e47
Rishi Raj <103919909+rishi-raj0104@users.noreply.github.com>
2024-12-03 14:45:10 +0530

    Update InputField.jsx

--- src/pages/dashboard/addBook/InputField.jsx
+++ src/pages/dashboard/addBook/InputField.jsx
@@ -9,6 +9,7 @@ const InputField = ({ label, name, type = 'text', register, placeholder }) => {
         {...register(name,  { required: true })}
         className=" p-2 border w-full rounded-md focus:outline-none focus:ring focus:border-blue-300"
         placeholder={placeholder}
+        step={type === 'number' ? 'any' : undefined}
       />
     </div>
   );
