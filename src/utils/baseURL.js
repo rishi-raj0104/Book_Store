326ea1a36a52ea044c8980ebf4b22d7fcfb2e3a8
Rishi Raj <rishi.rishiraj12@gmail.com>
2025-01-23 00:00:59 +0530

    error solve

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-    return "http://localhost:4000"
+    //return "http://localhost:4000"
+    return "https://book-store-backend-five-alpha.vercel.app/"
 }
 
 export default getBaseUrl;
